e9ddeb1e57b3e6356e20a47c41e10011f290b98b
Torjus Iveland <torjusti@gmail.com>
2016-08-29 18:50:20 +0200

    Lagt til ditt navn øving.

---
+++ assignments/1/ditt_navn.py
@@ -0,0 +1,7 @@
+def main():
+    navn = input("Navn: ")
+    alder = int(input("Alder: "))
+    print("Mitt navn er ", navn,", og jeg er ", alder, " år.", sep="")
+
+if __name__ == "__main__":
+    main()
