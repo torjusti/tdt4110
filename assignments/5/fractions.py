edd6e4970b405312cec30f357e6768400e98f245
Torjus Iveland <torjusti@gmail.com>
2016-10-10 19:58:03 +0200

    Add fractions.py

---
+++ assignments/5/fractions.py
@@ -0,0 +1,16 @@
+def gcd(a, b):
+    while b != 0: a, b = b, a % b
+    return a
+
+def reduceFraction(a, b):
+    factor = gcd(a, b)
+    return (a // factor), (b // factor)
+
+def main():
+    while True:
+        a = int(input('a:'))
+        b = int(input('b:'))
+        teller, nevner = reduceFraction(a, b)
+        print(teller, '/', nevner, sep='')
+
+main()
